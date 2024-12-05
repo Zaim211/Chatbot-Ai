3c7ec0ffe5b2aa4ea311639004036062259a0fb3
Zaim211 <yousseftitazaim@gmail.com>
2024-12-05 21:14:38 +0000

    update

--- src/chatbot-widget.jsx
+++ src/chatbot-widget.jsx
@@ -1,4 +1,18 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+// import Chatbot from './components/Chatbot';
+// import './index.css';
+
+// const createChatbotWidget = () => {
+//   const container = document.createElement("div");
+//   container.id = "chatbot-root";
+//   document.body.appendChild(container);
+
+//   ReactDOM.render(<Chatbot />, container);
+// };
+
+// window.createChatbotWidget = createChatbotWidget
+
+import ReactDOM from 'react-dom/client';
 import Chatbot from './components/Chatbot';
 import './index.css';
 
@@ -7,7 +21,8 @@ const createChatbotWidget = () => {
   container.id = "chatbot-root";
   document.body.appendChild(container);
 
-  ReactDOM.render(<Chatbot />, container);
+  const root = ReactDOM.createRoot(container);
+  root.render(<Chatbot />);
 };
 
-window.createChatbotWidget = createChatbotWidget
+window.createChatbotWidget = createChatbotWidget;
