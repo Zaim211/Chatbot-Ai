58b9c4c6e2b15ce47c1064af8a8b7e18cfe3a139
Zaim211 <yousseftitazaim@gmail.com>
2024-12-05 21:53:31 +0000

    test build

--- vite.config.js
+++ vite.config.js
@@ -28,37 +28,6 @@
 
 
 
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-
-// // Vite configuration for building the website and chatbot widget
-// export default defineConfig({
-//   plugins: [react()],
-//   assetsInclude: ['**/*.PNG'],
-//   define: {
-//     'process.env': {}, // This will define process.env as an empty object, preventing the error
-//   },
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src'), // Alias for imports
-//     },
-//   },
-//   build: {   
-//     lib: {
-//       entry: path.resolve(__dirname, 'src/chatbot-widget.jsx'),
-//       name: 'ChatbotWidget',
-//       fileName: (format) => `chatbot-widget.${format}.js`,
-//       formats: ['umd', 'es'], // UMD for global scope, ES for modern imports
-//     },
-//   },
-// })
-
-
-
-
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -75,20 +44,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'), // Alias for imports
     },
   },
-  build: {
-    // Build for both the chatbot widget and website
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'), // Your main website entry point
-        chatbotWidget: path.resolve(__dirname, 'src/chatbot-widget.jsx'), // Chatbot widget entry point
-      },
-      output: {
-        entryFileNames: '[name].js',
-        format: 'esm', // Ensuring the output is in modern format
-      },
+  build: {   
+    lib: {
+      entry: path.resolve(__dirname, 'src/chatbot-widget.jsx'),
+      name: 'ChatbotWidget',
+      fileName: (format) => `chatbot-widget.${format}.js`,
+      formats: ['umd', 'es'], // UMD for global scope, ES for modern imports
     },
   },
-});
+})
+
 
 
 
