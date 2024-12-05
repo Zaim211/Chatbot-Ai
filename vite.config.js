436d6afe1560720d3f3489e029fed08b455cfd20
Zaim211 <yousseftitazaim@gmail.com>
2024-12-05 16:41:09 +0000

    build chatbot

--- vite.config.js
+++ vite.config.js
@@ -42,5 +42,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG'],
+  build: {
+    outDir: 'dist', // Output folder for the build
+    assetsInclude: ['**/*.PNG'], // Include assets
+  },
 })
