b43a83a27515457359d5d2467ba4420b29639d98
Zaim211 <yousseftitazaim@gmail.com>
2024-12-11 21:35:54 +0000

    style button Réunion

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,12 +5,13 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { curve } from "../assets";
+import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const pathname = useLocation();
   const [openNavigation, setOpenNavigation] = useState(false);
-
 
   const toggleNavigation = () => {
     if (openNavigation) {
@@ -30,9 +31,8 @@ const Header = () => {
   };
 
   const handleRedirect = () => {
-    window.open('https://app.iclosed.io/e/Amar/rendez-vous', '_blank');
+    window.open("https://app.iclosed.io/e/Amar/rendez-vous", "_blank");
   };
-  
 
   return (
     <div
@@ -84,13 +84,26 @@ const Header = () => {
 
           <HamburgerMenu />
         </nav>
-        
-        
+
+        {/*         
         <Button
         onClick={handleRedirect}
-         className="hidden text-black lg:flex">
-          Schedule a Meeting
-        </Button>
+         className="bg-green-500 text-white font-bold text-md rounded-lg flex items-center  hover:bg-green-300 transition"
+        >
+          Planifier une Réunion
+        </Button> */}
+        <Button 
+  onClick={handleRedirect} 
+  className="bg-[#5ba85b] text-white font-bold hover:text-black text-md rounded-lg flex items-center hover:bg-[#e6ece6] transition"
+>
+  Planifier une Réunion
+  <FontAwesomeIcon 
+    icon={faCalendarAlt} 
+    style={{ fontSize: "20px" }} 
+    className="ml-2"  // Adds space to the left of the icon
+  />
+</Button>
+
 
         <Button
           className="ml-auto lg:hidden bg-black rounded-md"
