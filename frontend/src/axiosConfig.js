3ecfad6b5f7ee4e10dae8e29032f0dfb5894e0f1
Zaim211 <yousseftitazaim@gmail.com>
2024-12-12 19:10:57 +0000

    url backend

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://chatbot-dashboard-anh8.onrender.com',
+    baseURL: 'http://localhost:5000',
     headers: {
         'Content-Type': 'application/json',
     },
