26d4faaef8d752e98dcbc8e0eccc802ec71cf6da
Zaim211 <yousseftitazaim@gmail.com>
2025-01-22 17:40:35 +0000

    update

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -7,7 +7,7 @@ const Title = ({ className, title, text, tag }) => {
     >
       {tag && <TagLine className="mb-2 h1 font-bold text-center text-lg text-[#b1e2be] md:justify-center">{tag}</TagLine>}
       {title && <h2 className="h1 text-center">{title}</h2>}
-      {text && <p className="body-2 lg:text-center  text-start mr-0 px-4 lg:p-0 lg:px-4 mt-2">{text}</p>}
+      {text && <p className="body-2 text-center  mr-0 px-4 lg:p-0 lg:px-4 mt-2">{text}</p>}
     </div>
   );
 };
